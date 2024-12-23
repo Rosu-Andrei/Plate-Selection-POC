f475a6e2eb06f4fd104100809b4f6e4dd5aa2224
Rosu Andrei <andrei_rosu1986@yahoo.com>
2024-12-23 12:11:22 +0200

    electron - added comments to the renderer.js file.

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,9 +1,20 @@
+/**
+ * we need ipcRenderer from the electron module to make communications possible
+ * to the app.js process.
+ */
 const { ipcRenderer } = require('electron');
 
+/**
+ * this listener is used so that we make sure that the DOM is loaded
+ * with all necessary data before any element is accessing.
+ */
 document.addEventListener('DOMContentLoaded', () => {
   const tabsContainer = document.getElementById('tabs-container');
   const newTabButton = document.getElementById('new-tab-button');
-
+  /**
+   * listens for 'tabs-updated' message from the main process. Once it receives it,
+   * we create a div for each element of the tabs array received from the main process.
+   */
   ipcRenderer.on('tabs-updated', (event, { tabs, activeTabId }) => {
     tabsContainer.innerHTML = '';
 
@@ -13,7 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
       if (tab.id === activeTabId) {
         tabEl.classList.add('active');
       }
-
+      /**
+       * for each tab we set up the title, the x button and its functionality.
+       */
       const tabTitle = document.createElement('span');
       tabTitle.textContent = 'Plate Application'
       tabEl.appendChild(tabTitle);
@@ -26,7 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
         ipcRenderer.send('close-tab', tab.id);
       };
       tabEl.appendChild(closeBtn);
-
+      /**
+       * when the user clicks on a tab, we send to the main process the 'activate-tab' message with
+       * the tab id, so that that tab will be selected.
+       */
       tabEl.onclick = () => {
         ipcRenderer.send('activate-tab', tab.id);
       };
