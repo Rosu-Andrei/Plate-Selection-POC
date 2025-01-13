6eb13dd0771eb7d5bd6b7e35b52c860e975514f8
Rosu Andrei <andrei_rosu1986@yahoo.com>
2025-01-13 17:52:39 +0200

    tab-as-angular-app - moved PlateTabComponent logic to the ElectronService + added initially 4 tabs + solved selection bug.

--- src/app/plate-tabs/plate-tabs.component.ts
+++ src/app/plate-tabs/plate-tabs.component.ts
@@ -17,79 +17,33 @@ export type TabData = {
   styleUrls: ['./plate-tabs.component.css']
 })
 export class PlateTabsComponent {
-  /**
-   * the tabs array is used to keep track of the current existing tabs in the electron application.
-   */
-  tabs: TabData[] = [];
-  nextTabId: number = 2; // we set the next id of a future tab to 2 because we now the first tab has an id of 1
-  selectedTabId: number = 0;
 
-  constructor(private electronService: ElectronService) {
+  constructor(public electronService: ElectronService) {
   }
 
   /**
    * we create the first tab in the electron application for the tab with id 1
    */
   ngOnInit(): void {
-    this.createTab(1, 96);
-    this.tabs.push({id: 1, tabName: 'Tab 1', isSelected: true});
-    this.selectedTabId = 1;
+    this.electronService.initializeTabs();
   }
 
-  /**
-   * this method is called when the user clicks on the Add Tab button. It will increment the nextTabId,
-   * will add a new object in the tabs array and in the end will call the 'createTab' method
-   * that will send a message back to the main electron process.
-   */
-  addTab(plateSize: number = 96): void {
-    const newTabId = this.nextTabId++;
-    this.tabs.push({id: newTabId, tabName: 'Tab ' + newTabId, isSelected: true});
-    this.createTab(newTabId, plateSize);
-    let currentSelectedTab = this.tabs.find(tabData => tabData.id == this.selectedTabId);
-    if (currentSelectedTab == undefined) {
-      return;
-    }
-    currentSelectedTab.isSelected = false;
-    this.selectedTabId = newTabId;
+  addTab(): void {
+    this.electronService.addTab(96);
   }
 
-  /**
-   * method is responsible for removal of the tab by its id. It also sends a message back to the
-   * main electron process.
-   */
   removeTab(index: number): void {
-    const removedTabId = this.tabs[index].id;
-    this.tabs.splice(index, 1);
-    this.electronService.removeTab(removedTabId);
-    if (!this.tabs[index].isSelected) {
-      return;
-    } else {
-      this.selectedTabId = this.tabs[0].id;
-      this.tabs[0].isSelected = true;
+    const tab = this.electronService.getTabs()[index];
+    if (tab) {
+      this.electronService.removeTab(tab.id);
     }
-
   }
 
-  /**
-   * Captures the click event of a respective tab and send that information to the main electron process
-   */
   switchTab(tab: TabData): void {
-    this.electronService.switchTab(tab);
-    let currentSelectedTab = this.tabs.find(tabData => tabData.id == this.selectedTabId);
-    if (currentSelectedTab == undefined) {
-      return;
-    }
-    currentSelectedTab.isSelected = false;
-    this.selectedTabId = tab.id;
-    tab.isSelected = true;
-
+    this.electronService.switchTab(tab.id);
   }
 
-  /**
-   * this method sends a message to the electron main process to create a new tab
-   * to which will also add a new BrowserView for the respective tabId.
-   */
-  private createTab(tabId: number, plateSize: number): void {
-    this.electronService.createTab(tabId, plateSize);
+  getTabs(): TabData[] {
+    return this.electronService.getTabs();
   }
 }
--- src/app/services/electron.service.ts
+++ src/app/services/electron.service.ts
@@ -6,6 +6,12 @@ import {TabData} from "../plate-tabs/plate-tabs.component";
 })
 export class ElectronService {
   private ipcRenderer: any;
+  /**
+   * the tabs array is used to keep track of the current existing tabs in the electron application.
+   */
+  public tabs: TabData[] = [];
+  private nextTabId: number = 1;
+  private selectedTabId: number = 0;
 
   constructor() {
     if ((window as any)?.require) {
@@ -14,21 +20,104 @@ export class ElectronService {
     }
   }
 
-  public createTab(tabId: number, plateSize: number) {
+  /**
+   * this method is called in the ngOnInit of the tabs component. Is responsible for
+   * creating an initial tab for the electron application.
+   */
+  public initializeTabs(): void {
+    this.addTab();
+    for (let i = 0; i < 2; i++)
+      this.addTab(96);
+    this.addTab(384);
+  }
+
+  /**
+   * this method is called when the user clicks on the Add Tab button. It will increment the nextTabId,
+   * will add a new object in the tabs array and in the end will call the 'createTab' method
+   * that will send a message back to the main electron process.
+   */
+  public addTab(plateSize?: number): void {
+    const newTabId = this.nextTabId++;
+    const newTab: TabData = {
+      id: newTabId,
+      tabName: `Tab ${newTabId}`,
+      isSelected: true,
+    };
+    const currentSelectedTab = this.tabs.find(tab => tab.id === this.selectedTabId);
+    if (currentSelectedTab) {
+      currentSelectedTab.isSelected = false;
+    }
+
+    this.tabs.push(newTab);
+    this.selectedTabId = newTabId;
+
+    this.createTab(newTabId, plateSize);
+  }
+
+  public createTab(tabId: number, plateSize?: number) {
     if (this.ipcRenderer) {
       this.ipcRenderer.send('create-tab', {tabId, plateSize});
     }
   }
 
-  public removeTab(removedTabId: number) {
+  /**
+   * method is responsible for removal of the tab by its id. It also sends a message back to the
+   * main electron process.
+   */
+  public removeTab(tabId: number): void {
+    const tabIndex = this.tabs.findIndex(tab => tab.id === tabId);
+    if (tabIndex === -1) return;
+
+    this.tabs.splice(tabIndex, 1);
+
+    /**
+     * if the current selected tab is deleted, then the first tab will be marked as selected.
+     */
+    if (this.selectedTabId === tabId) {
+      if (this.tabs.length > 0) {
+        this.selectedTabId = this.tabs[0].id;
+        this.tabs[0].isSelected = true;
+      } else {
+        this.selectedTabId = 0;
+      }
+    }
     if (this.ipcRenderer) {
-      this.ipcRenderer.send('remove-tab', {tabId: removedTabId});
+      this.ipcRenderer.send('remove-tab', {tabId});
     }
   }
 
-  public switchTab(tab: TabData) {
-    if (this.ipcRenderer) {
-      this.ipcRenderer.send('switch-tab', {tabId: tab.id});
+  /**
+   * Captures the click event of a respective tab and send that information to the main electron process
+   */
+  public switchTab(tabId: number): void {
+    /**
+     * mark as deselect the tab that is currently selected
+     */
+    const currentSelectedTab = this.tabs.find(tab => tab.id === this.selectedTabId);
+    if (currentSelectedTab) {
+      currentSelectedTab.isSelected = false;
+    }
+
+    /**
+     * mark as selected the newly clicked tab.
+     */
+    const newSelectedTab = this.tabs.find(tab => tab.id === tabId);
+    if (newSelectedTab) {
+      newSelectedTab.isSelected = true;
+      this.selectedTabId = tabId;
+
+      if (this.ipcRenderer) {
+        this.ipcRenderer.send('switch-tab', {tabId});
+      }
     }
   }
+
+  /**
+   * return the TabData array.
+   */
+  public getTabs(): TabData[] {
+    return this.tabs;
+  }
+
+
 }
